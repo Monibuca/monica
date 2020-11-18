96b079e7d7d2dec4a1e90879e0d197042288d893
godkun <godkun666@126.com>
2020-11-18 22:57:33 +0800

    fix: 修复hellword动画,只在首页展示

--- server/src/components/HelloWorld.vue
+++ server/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
-  <img class="logo" alt="Vue logo" src="../assets/logo.png" />
-  <h1 class="msg">Monibuca可视化实例管理器</h1>
+  <fragment v-if="$route.path=='/'">
+    <img class="logo" alt="Vue logo" src="../assets/logo.png" />
+    <h1 class="msg">Monibuca可视化实例管理器</h1>
+  </fragment>
+  <fragment v-else>
+    <img class="logo-animation-end" alt="Vue logo" src="../assets/logo.png" />
+    <h1 class="msg-animation-end">Monibuca可视化实例管理器</h1>
+  </fragment>
 </template>
 
 <script>
@@ -14,6 +20,7 @@ export default {
     top: 10px;
     width: 32px;
     left: 10px;
+    transform: none;
   }
 }
 @keyframes msgA {
@@ -21,19 +28,34 @@ export default {
     top: 12px;
     left: 60px;
     font-size: 14px;
+    transform: none;
   }
 }
 .logo {
-  position: absolute;
   animation: logoA 0.5s ease 2s forwards;
+  position: absolute;
   top: 60px;
   width: 128px;
-  left: calc(50vw - 64px);
+  left: 50%;
+  transform: translateX(-50%);
 }
 .msg {
+  animation: msgA 0.5s ease 2s forwards;
   position: absolute;
   top: 260px;
-  left: calc(50vw - 138px);
-  animation: msgA 0.5s ease 2s forwards;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.logo-animation-end {
+  position: absolute;
+  top: 10px;
+  width: 32px;
+  left: 10px;
+}
+.msg-animation-end {
+  position: absolute;
+  top: 12px;
+  left: 60px;
+  font-size: 14px;
 }
 </style>
