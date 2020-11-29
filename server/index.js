f0260465b4672d692ad3f0b4a77092d1c5142c52
godkun <godkun666@126.com>
2020-11-29 19:52:08 +0800

    fix: 放开 shell.ln

--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ const instanceMap = new Map()
 const { koaEventStream } = require('fastrx/extention')
 const { rx, concat, catchError, map, pipe } = require('fastrx')
 const scriptExt = os.platform() == 'win32' ? 'bat' : 'sh'
-// shell.ln(
-//   '-sf',
-//   path.join(__dirname, '../node_modules'),
-//   path.join(__dirname, './node_modules')
-// )
+shell.ln(
+  '-sf',
+  path.join(__dirname, '../node_modules'),
+  path.join(__dirname, './node_modules')
+)
 process.chdir(__dirname)
 if (!fs.existsSync(instancesDir))
   fs.mkdirSync(instancesDir, { recursive: true })
